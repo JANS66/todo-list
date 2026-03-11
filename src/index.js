feebaba7204e5e3ad3fefac720ede4fd272df0c3
JANS66 <98973687+JANS66@users.noreply.github.com>
2026-03-11 10:42:59 +0200

    feat: create todo objects from gathered data, and display them

--- src/index.js
+++ src/index.js
@@ -17,12 +17,36 @@ cancelButton.addEventListener('click', () => {
 
 
 taskForm.addEventListener('submit', () => {
+    // Gather data
     const formData = new FormData(taskForm);
     const data = Object.fromEntries(formData.entries());
-    console.log("Success! Task Data:", data);
+
+    // Create todo
+    const todo = new Todo(data.title, data.description, data.dueDate, data.priority);
+
+    // Call function for displaying todo
+    displayTodo(todo);
+
     taskForm.reset();
 });
 
 dialog.addEventListener('click', (e) => {
     if (e.target === dialog) dialog.close();
 });
+
+const displayTodo = todo => {
+    const todoList = document.querySelector('#todo-list');
+
+    const card = document.createElement('li');
+
+    card.innerHTML = `
+        <div>
+            <span>${todo.title}</span>
+            <span>${todo.description}</span>
+            <span>${todo.priority}</span>
+            <span>${todo.dueDate}</span>
+        </div>
+    `
+
+    todoList.appendChild(card);
+}
